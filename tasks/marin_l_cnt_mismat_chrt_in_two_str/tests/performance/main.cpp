c7c4bfbfdbccb0270919d46053a03c2ac4201dd2
daddyjudah <moderator647@gmail.com>
2025-10-30 23:25:45 +0300

    Fix tests name

--- tasks/marin_l_cnt_mismat_chrt_in_two_str/tests/performance/main.cpp
+++ tasks/marin_l_cnt_mismat_chrt_in_two_str/tests/performance/main.cpp
@@ -7,7 +7,7 @@
 
 namespace marin_l_cnt_mismat_chrt_in_two_str {
 
-class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
+class MarinLCntMismatChrtInTwoStrPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
   const int kCount_ = 100;
   InType input_data_{};
 
@@ -24,7 +24,7 @@ class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, O
   }
 };
 
-TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+TEST_P(MarinLCntMismatChrtInTwoStrPerfTests, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
@@ -33,8 +33,8 @@ const auto kAllPerfTasks =
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+const auto kPerfTestName = MarinLCntMismatChrtInTwoStrPerfTests::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, MarinLCntMismatChrtInTwoStrPerfTests, kGtestValues, kPerfTestName);
 
 }  // namespace marin_l_cnt_mismat_chrt_in_two_str
