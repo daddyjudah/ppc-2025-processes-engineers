c294e8b161a6b7b42f7fa5949112d4fd7ce10800
daddyjudah <moderator647@gmail.com>
2025-11-03 17:53:46 +0300

    Changed common

--- tasks/marin_l_cnt_mismat_chrt_in_two_str/common/include/common.hpp
+++ tasks/marin_l_cnt_mismat_chrt_in_two_str/common/include/common.hpp
@@ -7,11 +7,14 @@
 
 namespace marin_l_cnt_mismat_chrt_in_two_str {
 
-using InType = int;
+using InType = std::pair<std::string, std::string>;
+
 using OutType = int;
+
 using TestType = std::tuple<int, std::string>;
+
 using BaseTask = ppc::task::Task<InType, OutType>;
 
-// static const char* PPC_ID_marin_l_cnt_mismat_chrt_in_two_str = "marin_l_cnt_mismat_chrt_in_two_str";
+ static const char* PPC_ID_marin_l_cnt_mismat_chrt_in_two_str = "marin_l_cnt_mismat_chrt_in_two_str";
 
 }  // namespace marin_l_cnt_mismat_chrt_in_two_str
