875ee77806d9ab75378039a35344befc0e93bcd6
daddyjudah <moderator647@gmail.com>
2025-11-13 20:18:44 +0300

    fix: change logic in ops_mpi.cpp runimpl

--- tasks/marin_l_cnt_mismat_chrt_in_two_str/mpi/src/ops_mpi.cpp
+++ tasks/marin_l_cnt_mismat_chrt_in_two_str/mpi/src/ops_mpi.cpp
@@ -41,36 +41,28 @@ bool MarinLCntMismatChrtInTwoStrMPI::RunImpl() {
   const std::string& s1 = GetInput().first;
   const std::string& s2 = GetInput().second;
 
-  size_t n = std::min(s1.size(), s2.size());
-  size_t chunk = n / size;
+  size_t max_len = std::max(s1.size(), s2.size());
+  size_t chunk = max_len / size;
   size_t start = rank * chunk;
-  size_t end = (rank == size - 1) ? n : start + chunk;
+  size_t end = (rank == size - 1) ? max_len : start + chunk;
 
   int local_count = 0;
   for (size_t i = start; i < end; i++) {
-    if (s1[i] != s2[i])
+    char c1 = (i < s1.size()) ? s1[i] : '\0';
+    char c2 = (i < s2.size()) ? s2[i] : '\0';
+    
+    if (c1 != c2) {
       local_count++;
+    }
   }
 
   int global_count = 0;
   MPI_Reduce(&local_count, &global_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
   if (rank == 0) {
-    int diff_len = static_cast<int>(
-        std::max(s1.size(), s2.size()) - std::min(s1.size(), s2.size()));
-    GetOutput() = global_count + diff_len;
+    GetOutput() = global_count;
   }
-  return true;
-}
-
-bool MarinLCntMismatChrtInTwoStrMPI::PostProcessingImpl() {
-  int finalized;
-  MPI_Finalized(&finalized);
-  if (!finalized) {
-    MPI_Finalize();
-  }
-  
-  return GetOutput() >= 0;
+  return true;;
 }
 
 }  // namespace marin_l_cnt_mismat_chrt_in_two_str
