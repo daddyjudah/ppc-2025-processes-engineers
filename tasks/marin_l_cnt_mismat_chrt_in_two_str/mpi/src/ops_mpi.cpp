9616b48699415f840bfc364f96965d2879bb1f43
daddyjudah <moderator647@gmail.com>
2025-12-07 14:25:30 +0300

    modify logic ops_mpi

--- tasks/marin_l_cnt_mismat_chrt_in_two_str/mpi/src/ops_mpi.cpp
+++ tasks/marin_l_cnt_mismat_chrt_in_two_str/mpi/src/ops_mpi.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <cstddef>
 #include <string>
+#include <vector>
 
 #include "marin_l_cnt_mismat_chrt_in_two_str/common/include/common.hpp"
 
@@ -31,26 +32,51 @@ bool MarinLCntMismatChrtInTwoStrMPI::RunImpl() {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  const std::string &s1 = GetInput().first;
-  const std::string &s2 = GetInput().second;
+  const std::string &s1_full = GetInput().first;
+  const std::string &s2_full = GetInput().second;
 
-  size_t max_len = std::max(s1.size(), s2.size());
+  size_t max_len = std::max(s1_full.size(), s2_full.size());
+
+  if (max_len == 0) {
+    GetOutput() = 0;
+    return true;
+  }
+
+  std::vector<int> sendcounts(size);
+  std::vector<int> displs(size);
+
   size_t chunk = max_len / size;
-  size_t start = rank * chunk;
-  size_t end = (rank == size - 1) ? max_len : start + chunk;
+  size_t rem = max_len % size;
+
+  size_t offset = 0;
+  for (int i = 0; i < size; i++) {
+    size_t len = chunk + (i < rem ? 1 : 0);
+    sendcounts[i] = static_cast<int>(len);
+    displs[i] = static_cast<int>(offset);
+    offset += len;
+  }
+
+  std::string s1_local(sendcounts[rank], '\0');
+  std::string s2_local(sendcounts[rank], '\0');
+
+  MPI_Scatterv(s1_full.data(), sendcounts.data(), displs.data(), MPI_CHAR, s1_local.data(), sendcounts[rank], MPI_CHAR,
+               0, MPI_COMM_WORLD);
+
+  MPI_Scatterv(s2_full.data(), sendcounts.data(), displs.data(), MPI_CHAR, s2_local.data(), sendcounts[rank], MPI_CHAR,
+               0, MPI_COMM_WORLD);
 
   int local_count = 0;
-  for (size_t i = start; i < end; i++) {
-    char c1 = (i < s1.size()) ? s1[i] : '\0';
-    char c2 = (i < s2.size()) ? s2[i] : '\0';
-
+  for (size_t i = 0; i < s1_local.size(); i++) {
+    char c1 = s1_local[i];
+    char c2 = s2_local[i];
     if (c1 != c2) {
       local_count++;
     }
   }
 
   int global_count = 0;
-  MPI_Allreduce(&local_count, &global_count, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+  MPI_Reduce(&local_count, &global_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&global_count, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
   GetOutput() = global_count;
   return true;
